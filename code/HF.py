3436bd83fce74456d5aeaf290eefa28e3f8bb0a2
schraderSimon <simonsch@student.matnat.uio.no>
2021-04-24 15:24:29 +0200

    Added densities

--- code/HF.py
+++ code/HF.py
@@ -130,20 +130,67 @@ def compute_energy(C,F,system,number_electrons,number_basissets):
         for nu in range(2*number_basissets):
             energy+=P[nu,mu]*(system.h[nu,mu]+F[nu,mu])
     return energy*0.5
-
+def find_psquared(system,C,number_basissets,num_grid_points):
+    spin_up=system.spf[::2]
+    spin_down=system.spf[1::2]
+    C_up=C[::2,:]
+    C_down=C[1::2,:]
+    wf_up=np.zeros((2*l,num_grid_points),dtype=np.complex128)
+    wf_down=np.zeros((2*l,num_grid_points),dtype=np.complex128)
+    print(C_up.shape)
+    print(spin_up.shape)
+    for i in range(2*number_basissets):
+        for k in range(number_basissets):
+            wf_up[i]+=(C_up[k,i]*spin_up[k])
+            wf_down[i]+=(C_down[k,i]*spin_down[k])
+    #I think there's a bug here, but I got to ask Øyvind about it!
+    densities=(np.abs(wf_up)**2+np.abs(wf_down)**2)* 2 #times two because of the bug!
+    return densities
 np.set_printoptions(precision=4)
 l=10
 grids_length=10
-num_grid_points=1001
+num_grid_points=101
 omega=0.25
 a=0.25
 odho = ODQD(l, grids_length, num_grid_points, a=a, alpha=1, potential=ODQD.HOPotential(omega))
 number_electrons=2
 anti_symmetrize=True
 system=GeneralOrbitalSystem(n=number_electrons,basis_set=odho,anti_symmetrize=anti_symmetrize)
+print(system.spf)
 print("Reference energy: ",(system.compute_reference_energy()))
 C=np.eye(2*l) #Create an initial guess for the coefficients
-F,epsilon,C,converged=solve_DIIS(system,number_electrons,l,anti_symmetrize=anti_symmetrize,tol=1e-5,maxiter=30,C=C)
+F,epsilon,C,converged=solve_DIIS(system,2,l,anti_symmetrize=anti_symmetrize,tol=1e-5,maxiter=100,C=C)
 groundstate=C[:,0] #First column
+print(np.dot(groundstate,groundstate),np.sum(groundstate))
 print("Converged: ",converged)
 print("Energy: ",compute_energy(C,F,system,number_electrons,l))
+newbasis=np.zeros((2*l,num_grid_points),dtype=np.complex128)
+for i in range(2*l):
+    for k in range(2*l):
+        newbasis[i]+=(C[i,k]*system.spf[k])
+fig = plt.figure(figsize=(16, 10))
+potential = odho.HOPotential(omega=omega)
+densities=find_psquared(system,C,l,num_grid_points)
+plt.plot(system.grid, potential(system.grid))
+for i in range(2*l):
+    plt.plot(
+        system.grid,
+        np.abs(densities[i]) + system.h[i, i].real,
+        label=r"$\chi_{" + f"{i}" + r"}$",
+    )
+
+plt.grid()
+plt.legend()
+plt.show()
+print(0.01*np.sum(densities[0]))
+print(0.01*np.sum(np.abs(system.spf[0]) ** 2))
+"""verification"""
+ground_state_density=0.5*(densities[0]+densities[1])
+fig, ax = plt.subplots(figsize=(16, 10))
+ax.plot(system.grid,ground_state_density)
+ax.set_xlim(-6,6)
+ax.set_ylim(0,0.4)
+im=plt.imread("ref.png")
+ax.imshow(im,extent=[-6,6,0,0.4],aspect='auto')
+plt.show()
+print(0.01*np.sum(ground_state_density))

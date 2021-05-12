8d573f2fd88806ecc8faa9ade7d4d616865a9eb9
schraderSimon <simonsch@student.matnat.uio.no>
2021-05-12 10:55:33 +0200

    added plotting of GHF/RHF Time-dependent overlap

--- code/Python/GHFSolver.py
+++ code/Python/GHFSolver.py
@@ -3,7 +3,7 @@ from quantum_systems import ODQD, GeneralOrbitalSystem
 import scipy
 from helper_functions import *
 class GHFSolverSystem(object):
-    """
+    '''
     General solver class for general Hartree Fock with HO-basis (1D) on a grid
 
     Attributes
@@ -39,12 +39,11 @@ class GHFSolverSystem(object):
 
     Methods
     ---------
-
-    """
+    '''
 
     def __init__(self,number_electrons=2,number_basisfunctions=10,
                 grid_length=10,num_grid_points=1001,omega=0.25,a=0.25):
-        """Initialize
+        """Initialize"""
         self.l=2*number_basisfunctions #Hartree Fock basis is twice the number of basis functions
         self.grid_length=grid_length
         self.num_grid_points=num_grid_points
@@ -52,14 +51,15 @@ class GHFSolverSystem(object):
         self.omega=omega
         self.a=a
         self.number_electrons=number_electrons
-        odho = ODQD(number_basisfunctions, grid_length, num_grid_points, a=a, alpha=1, potential=ODQD.HOPotential(omega))
+        self.odho = ODQD(number_basisfunctions, grid_length, num_grid_points, a=a, alpha=1, potential=ODQD.HOPotential(omega))
         anti_symmetrize=True
-        self.system=GeneralOrbitalSystem(n=number_electrons,basis_set=odho,
+        self.system=GeneralOrbitalSystem(n=number_electrons,basis_set=self.odho,
                                         anti_symmetrize=anti_symmetrize)
     def setC(self,C=None):
         if C is None:
             self.C=np.random.rand(self.l,self.l) #Random matrix (possibly illegal)
-        self.C=C
+        else:
+            self.C=C
     def construct_Density_matrix(self,C):
         slicy=slice(0,self.number_electrons)
         return np.einsum("ma,va->mv",C[:,slicy],C.conjugate()[:,slicy])
@@ -77,16 +77,15 @@ class GHFSolverSystem(object):
                     converged=True
                     break
             P_old=P
-            epsilon, self.C = scipy.linalg.eigh(F)
+            self.epsilon, self.C = scipy.linalg.eigh(F)
             P=self.construct_Density_matrix(self.C)
         return converged
 
     def get_energy(self):
         energy=0
-        P=construct_Density_matrix(self.C)
+        P=self.construct_Density_matrix(self.C)
         tot_vec=self.system.h+self.construct_Fock_matrix(P)
         energy=0.5*np.einsum("nm,mn->",P,tot_vec)
-        print(energy)
         return energy
     def getDensity(self):
         spin_up=self.system.spf[::2]
@@ -146,3 +145,25 @@ class GHFSolverSystem(object):
     def reset_time(self):
         self.C=self.C0
         self.setUpIntegrator()
+        print("test")
+
+class RHFSolverSystem(GHFSolverSystem):
+    def __init__(self,number_electrons=2,number_basisfunctions=10,
+                grid_length=10,num_grid_points=1001,omega=0.25,a=0.25):
+        self.l=number_basisfunctions #Hartree Fock basis is twice the number of basis functions
+        self.grid_length=grid_length
+        self.num_grid_points=num_grid_points
+        self.steplength=(grid_length*2)/(num_grid_points-1) #Step length for Numerical integration or similar
+        self.omega=omega
+        self.a=a
+        self.number_electrons=number_electrons
+        self.system=ODQD(number_basisfunctions, grid_length, num_grid_points, a=a, alpha=1, potential=ODQD.HOPotential(omega))
+    def construct_Density_matrix(self,C):
+        slicy=slice(0,int(self.number_electrons/2))
+        return np.einsum("ma,va->mv",C[:,slicy],C.conjugate()[:,slicy])
+    def construct_Fock_matrix(self,P):
+        udirect=np.einsum("ts,msnt->mn",P,self.system.u)
+        uexchange=np.einsum("ts,mstn->mn",P,self.system.u)
+        return self.system.h+udirect*2-uexchange
+    def get_energy(self):
+        return super().get_energy()*2
--- code/Python/HF_objectOriented.py
+++ code/Python/HF_objectOriented.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 from quantum_systems import ODQD, GeneralOrbitalSystem
 from helper_functions import integrate_trapezoidal
 import scipy
+import sys
 import seaborn as sns
 from numpy import sin, pi
 from GHFSolver import *
-def wrapperFunction(t,T=10*pi):
+np.set_printoptions(precision=1,linewidth=250)
+def wrapperFunction(t,T=20*pi):
     returnval=sin(pi*t/T)**2*(t<T)
     return returnval
 def timedependentPotential(t,somega=2,E=1):
@@ -19,50 +21,185 @@ def total_Potential(t):
 number_electrons=2;
 grid_length=10
 num_grid_points=1001
-omega=1
+omega=0.25
 a=0.25
 l=10
-solver =GHFSolverSystem(number_electrons,l,grid_length,num_grid_points,omega,a);
-solver.setC(np.eye(2*l));
-solver.solve(1e-16,25)
-def plot_groundstate_density(solver):
+def plot_Comparison_HF(amount):
+    energy_RHF=np.zeros(amount)
+    energy_GHF=np.zeros(amount)
+    solver=RHFSolverSystem(number_electrons,l,grid_length,num_grid_points,omega,a);
+    solver.setC(np.eye(l));
+    for i in range(amount):
+        solver.solve(1e-14,1)
+        energy_RHF[i]=solver.get_energy()
+        print(i,energy_RHF[i])
+    solver =GHFSolverSystem(number_electrons,l,grid_length,num_grid_points,omega,a);
+    solver.setC(np.eye(2*l));
+    for i in range(amount):
+        solver.solve(1e-14,1)
+        energy_GHF[i]=solver.get_energy()
+        print(i,energy_GHF[i])
+    sns.set_style("darkgrid")
+    sns.set_context("talk")
+
+    plt.plot(energy_RHF,label="RHF")
+    plt.plot(energy_GHF,label="GHF")
+    plt.legend()
+    plt.xlabel("Number of steps in SCF algorithm")
+    plt.ylabel("Energy (Hartree)")
+    plt.tight_layout()
+    plt.savefig("../../figures/comparison_RHF_GHF.pdf")
+    plt.show()
+#plot_Comparison_HF(5000)
+
+
+def plot_groundstate_densities():
+    solver =GHFSolverSystem(number_electrons,l,grid_length,num_grid_points,omega,a);
+    solver.setC(np.eye(2*l));
+    solver.solve(1e-14,25)
     densities=solver.getDensity()
-    ground_state_density=(densities[0]+densities[1])
+    ground_state_density_RHF=(densities[0]+densities[1])
+    solver.solve(1e-14,5000)
+    densities=solver.getDensity()
+    ground_state_density_GHF=(densities[0]+densities[1])
     grid=solver.system.grid
-    fig, ax = plt.subplots(figsize=(16, 10))
-    ax.plot(grid,ground_state_density,label="HF ground state density")
+    sns.set_style("darkgrid")
+    sns.set_context("talk")
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.plot(grid,ground_state_density_RHF,label="RHF ground state density")
+    ax.plot(grid,ground_state_density_GHF,label="GHF ground state density")
     ax.set_xlim(-6,6)
-    ax.set_ylim(0,0.6)
-    im=plt.imread("ref.png")
-    ax.imshow(im,extent=[-6,6,0,0.4],aspect='auto')
+    ax.set_ylim(0,0.4)
+    #im=plt.imread("ref.png")
+    #ax.imshow(im,extent=[-6,6,0,0.4],aspect='auto')
+
     plt.xlabel("distance [a.u.]")
-    plt.ylabel(r"electron density $\rho$")
+    plt.ylabel(r"electron density $2\rho(x)$")
     plt.legend()
     plt.xlabel("Position [a.u.]")
     plt.ylabel(r"Total electron density $\rho$")
-    plt.savefig("../figures/total_density.pdf")
+    plt.tight_layout()
+    plt.savefig("../../figures/total_density.pdf")
     plt.show()
-plot_groundstate_density(solver)
-t=np.linspace(0,20*pi,5000)
-overlap=np.zeros(len(t))
-dipole_moment=np.zeros(len(t),dtype=np.complex128)
-energies=np.zeros(len(t))
-dt=t[1]-t[0]
-ground_state_densities=np.zeros((num_grid_points,len(t)))
-solver.init_TDHF(total_Potential)
-for i,tval in enumerate(t):
-    solver.integrate(dt) #Integrate a step dt
+#plot_groundstate_densities()
+
+def plot_molecular_orbitals():
+    colors=["blue","orange","green","red"]
+    potential=ODQD.HOPotential(omega=omega)
+    sns.set_style("darkgrid")
+    sns.set_context("talk")
+    fig, (ax1, ax2) = plt.subplots(1, 2,sharey=True,figsize=(15, 10))
+    solver =GHFSolverSystem(number_electrons,l,grid_length,num_grid_points,omega,a);
+    grid=solver.system.grid
+    solver.setC(np.eye(2*l));
+    solver.solve(1e-14,25)
     densities=solver.getDensity()
-    ground_state_densities[:,i]=densities[0]
-    overlap[i]=solver.calculate_overlap()
-    dipole_moment[i]=solver.getDipolemoment()
-grid=solver.system.grid
+    eigenvalues=solver.epsilon
+    for i in range(0,4,1):
+        ax1.axhline(eigenvalues[i].real,linestyle="--",color=colors[i])
+        ax1.plot(
+            grid,
+            densities[i] + eigenvalues[i].real,
+            label=r"$\rho_{" + f"{i}" + r"}$",color=colors[i]
+        )
+    solver =GHFSolverSystem(number_electrons,l,grid_length,num_grid_points,omega,a);
+    solver.setC(np.eye(2*l));
+    solver.solve(1e-14,5000)
+    densities=solver.getDensity()
+    eigenvalues=solver.epsilon
+    for i in range(0,4,1):
+        ax2.axhline(eigenvalues[i].real,linestyle="--",color=colors[i])
+        ax2.plot(
+            grid,
+            densities[i] + eigenvalues[i].real,
+            label=r"$\rho_{" + f"{i}" + r"}$",color=colors[i]
+        )
+    ax1.set_ylim(0,2)
+    ax1.set_xlim(-10,10)
+    ax2.set_xlim(-10,10)
+    ax1.plot(grid,potential(grid),color="violet",linestyle="dotted")
+    ax2.plot(grid,potential(grid),color="violet",linestyle="dotted")
+    ax1.set_ylabel("Energy [Hartree] + electron density")
+    ax1.set_xlabel("Position [a.u.]")
+    ax2.set_xlabel("Position [a.u.]")
+    ax1.set_title("RHF solution")
+    ax2.set_title("GHF solution")
+    ax1.legend()
+    ax2.legend()
+    plt.savefig("../../figures/MO_densities.pdf")
+    plt.show()
+#plot_molecular_orbitals()
+
+def plot_time_evolution(time_potenial):
+    sns.set_style("darkgrid")
+    sns.set_context("talk")
+    fig, ax = plt.subplots(figsize=(16, 4))
+    ax.set_ylim(0,1)
+    ax.set_xlim(0,16)
+    im=plt.imread("zanghellini2.png")
+    #ax.imshow(im,extent=[0,4,0,1],aspect='auto')
+    t=np.linspace(0,16*pi,2000)
+    solver =GHFSolverSystem(number_electrons,l,grid_length,num_grid_points,omega,a);
+    solver.setC(np.eye(2*l));
+    solver.solve(1e-14,25)
+    grid=solver.system.grid
+    overlap=np.zeros(len(t))
+    dipole_moment=np.zeros(len(t),dtype=np.complex128)
+    energies=np.zeros(len(t))
+    dt=t[1]-t[0]
+    ground_state_densities=np.zeros((num_grid_points,len(t)))
+    solver.init_TDHF(time_potenial)
+    overlap[0]=solver.calculate_overlap()
+    dipole_moment[0]=solver.getDipolemoment()
+    for i,tval in enumerate(t[:-1]):
+        solver.integrate(dt) #Integrate a step dt
+        densities=solver.getDensity()
+        ground_state_densities[:,i]=densities[0]
+        overlap[i+1]=solver.calculate_overlap()
+        dipole_moment[i+1]=solver.getDipolemoment()
+    ax.plot(2*t/(2*pi),overlap,label="RHF")
+    solver =GHFSolverSystem(number_electrons,l,grid_length,num_grid_points,omega,a);
+    solver.setC(np.eye(2*l));
+    solver.solve(1e-14,5000)
+    grid=solver.system.grid
+    overlap=np.zeros(len(t))
+    dipole_moment=np.zeros(len(t),dtype=np.complex128)
+    energies=np.zeros(len(t))
+    dt=t[1]-t[0]
+    ground_state_densities=np.zeros((num_grid_points,len(t)))
+    solver.init_TDHF(time_potenial)
+    overlap[0]=solver.calculate_overlap()
+    dipole_moment[0]=solver.getDipolemoment()
+    for i,tval in enumerate(t[:-1]):
+        solver.integrate(dt) #Integrate a step dt
+        densities=solver.getDensity()
+        ground_state_densities[:,i]=densities[0]
+        overlap[i+1]=solver.calculate_overlap()
+        dipole_moment[i+1]=solver.getDipolemoment()
+    ax.plot(t/pi,overlap,label="GHF")
+
+    plt.legend()
+    ax.set_xlabel(r"t $\omega/(2\pi)$")
+    ax.set_ylabel(r"Overlap $|\langle\Psi(t)|\Psi(0)\rangle|^2$")
+    plt.tight_layout()
+    plt.savefig("../../figures/time_overlap.pdf")
+    plt.show()
+plot_time_evolution(timedependentPotential)
+sys.exit(1)
+
+
+
+
+
+sns.set_style("darkgrid")
+sns.set_context("talk")
 plt.plot(t,dipole_moment)
 plt.show()
-sp=np.split(np.fft.fft(dipole_moment[2500:]),2)[0]
+sp=np.split(np.fft.fft(dipole_moment[2000:]),2)[0]
 #sp=np.split(np.fft.fft(np.sin(2*np.pi*t[1500:]+1)),2)[0]
-freq=np.split(np.fft.fftfreq(len(dipole_moment[2500:]),d=dt),2)[0]*(2*pi)
+freq=np.split(np.fft.fftfreq(len(dipole_moment[2000:]),d=dt),2)[0]*(2*pi)
 plt.plot(freq,sp.real,freq,sp.imag)
+plt.plot(freq,np.abs(sp))
 #plt.xlim(-1,1)
 plt.show()
 
@@ -92,4 +229,4 @@ def animate():
     ani = animation.FuncAnimation(fig, animate, np.arange(0,len(t)), init_func=init,
                                   interval=20, blit=True)
     plt.show()
-animate()
+#animate()
